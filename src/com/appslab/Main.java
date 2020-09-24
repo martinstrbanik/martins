fd60d4451e177baf0d92623939184655bb46e8c7
Martin3 <strbanik.jun@gmail.com>
2020-09-24 17:45:20 +0200

    Cvicenie 2 equality

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -1,8 +1,41 @@
 package com.appslab;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello AppsLab 2020! haa");
+        Scanner input = new Scanner(System.in);
+        System.out.print("Zadajte cislo a: ");
+        int a = input.nextInt();
+        System.out.print("Zadajte cislo b: ");
+        int b = input.nextInt();
+        System.out.print("Zadajte cislo c: ");
+        int c = input.nextInt();
+        input.close();
+
+        System.out.print("Pocet rovnakych cisiel: ");
+        if (a==b && b==c)
+        {
+            System.out.println(3);
+        }
+
+        else if (a==c)
+        {
+            System.out.println(2);
+        }
+        else if (a==b)
+        {
+            System.out.println(2);
+        }
+        else if (c==b)
+        {
+            System.out.println(2);
+        }
+        else
+        {
+            System.out.println(0);
+        }
+
+
     }
 }
