8b88f858b61607a43009d9a39ccdc828b206120c
openlab <openlab@z00p1-17.SPSKNM>
2020-09-29 09:14:24 +0200

    Complete exercise 4

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -1,8 +1,22 @@
 package com.appslab;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello AppsLab 2020! haa");
+        Scanner input = new Scanner(System.in);
+        System.out.print("Please enter how many inputs you want: ");
+        int b=input.nextInt();
+        int[] a = new int[b];
+
+        for (int i = 0; i < a.length; i++)
+        {
+            System.out.println("Please enter number");
+            a[i] = input.nextInt();
+        }
+
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i]*a.length);
+        }
     }
 }
