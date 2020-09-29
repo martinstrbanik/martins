a2de67f695052b605a6689d9a5d3f5995cdf9009
openlab <openlab@z00p1-17.SPSKNM>
2020-09-29 09:30:44 +0200

    Complete exercise 5

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -1,8 +1,25 @@
 package com.appslab;
-
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello AppsLab 2020! haa");
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Input total number of slices: ");
+        int tSlices=input.nextInt();
+        System.out.print("Input total number of recipients: ");
+        int N0Rec=input.nextInt();
+        System.out.print("Input number of slices per recipient: ");
+        int slicePerRec=input.nextInt();
+
+        if(N0Rec*slicePerRec<=tSlices)
+        {
+            System.out.print("True");
+
+        }
+        else
+            {
+                System.out.print("False");
+            }
     }
 }
