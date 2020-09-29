3970b6317a22dfd5eb85fd634ac5ba7eb248c818
openlab <openlab@z00p1-17.SPSKNM>
2020-09-29 08:36:28 +0200

    Uloha 3 trojuholnik

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -1,8 +1,33 @@
 package com.appslab;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello AppsLab 2020! haa");
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("zadajte stranu a: ");
+        int a=input.nextInt();
+        System.out.print("zadajte stranu b: ");
+        int b=input.nextInt();
+        System.out.print("zadajte stranu c: ");
+        int c=input.nextInt();
+        input.close();
+
+        if (c<b||c<a)
+        {
+            System.out.print("strana c musi byt najvacsia");
+        }
+        else if (a+b>c)
+        {
+            System.out.println("Trojuholnik je mozny.");
+        }
+
+        else{
+            System.out.println("Trojuholnik je nemozny");
+        }
+
+
+
     }
 }
