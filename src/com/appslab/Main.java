678180283142ecfe385b90e6ffaf65c2c93bfdaa
openlab <openlab@z00p1-17.SPSKNM>
2020-10-06 08:35:21 +0200

    Exercise 12b finished

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -3,6 +3,21 @@ package com.appslab;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello AppsLab 2020! haa");
+        Student Student1 = new Student();
+        Student Student2 = new Student();
+
+        Student1.name = "John";
+        Student1.studentNumber = 2;
+        Student1.address = "Legionarska 1, 01001 Zilina";
+        Student1.phoneNumber = 123456;
+
+        Student2.name = "Sam";
+        Student2.studentNumber = 5;
+        Student2.address = "Legionarska 2, 01001 Zilina";
+        Student2.phoneNumber = 456789;
+
+
+        System.out.println(Student1.getAddress() + " " + Student1.getPhoneNumber() + " " + Student1.getStudentNumber() );
+        System.out.println(Student2.getAddress() + " " + Student2.getPhoneNumber() + " " + Student2.getStudentNumber() );
     }
 }
---
+++ src/com/appslab/Student.java
@@ -0,0 +1,24 @@
+package com.appslab;
+
+public class Student {
+
+    int studentNumber;
+    int phoneNumber;
+    String address;
+    String name;
+
+    int getStudentNumber(){
+        return studentNumber;
+    }
+
+    int getPhoneNumber(){
+        return phoneNumber;
+    }
+
+    String getAddress(){
+        return address;
+    }
+    String getName(){
+        return name;
+    }
+}
