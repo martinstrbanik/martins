922857b4ec07a92f496e08929e515ba13bd8bd19
openlab <openlab@z00p1-17.SPSKNM>
2020-10-06 09:37:19 +0200

    Exercise 13 finished

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -3,6 +3,26 @@ package com.appslab;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello AppsLab 2020! haa");
+        Person people[] = new Person[3];
+        Person person1 = new Person(12, "John", 1000);
+        Person person2 = new Person(20, "Steve", 2000);
+        Person person3 = new Person (30, "Martin", 3000);
+
+        people[0] = person1;
+        people[1] = person2;
+        people[2] = person3;
+
+
+        System.out.println(calculateBudged(people));
+
+
     }
+    public static int calculateBudged(Person[] people){
+        int budget = 0;
+        for (int i = 0; i< people.length; i++) {
+          budget +=people[i].budget;
+        }
+        return budget;
+    }
+
 }
