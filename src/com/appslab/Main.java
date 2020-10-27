9c42c517e551a223c54e2ffe3715c7ca9e9a1b08
Martin3 <strbanik.jun@gmail.com>
2020-10-27 16:44:37 +0100

    1. task from collections finished

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -1,8 +1,34 @@
 package com.appslab;
 
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello AppsLab 2020! haa");
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        list.add(1);
+        list.add(1);
+        list.add(1);
+        list.add(2);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+
+        out(list);
     }
+
+    static void out(ArrayList<Integer>list){
+        if (list.size()==0){
+            System.out.println("ArrayList is empty");
+        }
+        else{
+            list.forEach((i)->{
+                System.out.println(i);
+            });
+        }
+    }
+
 }
+
+
