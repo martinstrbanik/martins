c4d166deacec7bcb2d5708e224773a0e14274e88
Martin3 <strbanik.jun@gmail.com>
2020-11-03 16:33:29 +0100

    1,2,3 task from collections finished

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -16,19 +16,48 @@ public class Main {
         list.add(5);
 
         out(list);
+        addIfNotExists(1,list);
+        addIfNotExists(6,list);
+        addIfNotExists(14,list);
+        addIfNotExists(5,list);
+        out(list);
+        evenNumberDelete(list);
+        out(list);
     }
 
     static void out(ArrayList<Integer>list){
+
         if (list.size()==0){
-            System.out.println("ArrayList is empty");
+            System.out.println("List is empty");
         }
         else{
-            list.forEach((i)->{
-                System.out.println(i);
-            });
+            System.out.println("List:");
+            list.forEach(System.out::println);
         }
     }
+
+    static void addIfNotExists(int a,ArrayList<Integer>list){
+        if(list.contains(a)){
+            System.out.println(a + " is already on the list and it won´t be added");
+        }
+        else{
+            System.out.println("Adding " + a + " to the list");
+            list.add(a);
+        }
+    }
+
+    static void evenNumberDelete(ArrayList<Integer>list){
+        System.out.println("Deleting even numbers...");
+        for(int i=0;i<list.size();i++){
+            if(list.get(i)%2==0){
+                list.remove(i);
+                i--;
+            }
+        }
+    }
+
 
 }
 
 
+    
