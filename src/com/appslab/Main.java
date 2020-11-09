cd4eed792b423d05464a6509c17dcc8cfc4bab78
Martin3 <strbanik.jun@gmail.com>
2020-11-09 15:55:35 +0100

    BE collections exercise 5 to be finished

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -1,8 +1,20 @@
 package com.appslab;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello AppsLab 2020! haa");
+        int[] a;
+        multiplyByLength(new int[5]);
+    }
+
+
+    public static int multiplyByLength(int[] a){
+        ArrayList<Integer> numbers = new ArrayList<Integer>();
+        numbers.add(a);
+        System.out.println(numbers);
+        return 0;
     }
 }
