c83f9e44f46cc9a3ee7fee1be477bc079a185b74
Martin3 <strbanik.jun@gmail.com>
2020-11-17 16:20:18 +0100

    BE collections exercise 8,9,10 finished

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -1,8 +1,40 @@
 package com.appslab;
+import java.util.HashMap;
+import java.util.HashSet;
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello AppsLab 2020! haa");
+        HashSet<Integer> list = new HashSet<Integer>();
+        list.add(1);
+        list.add(1);
+        list.add(1);
+        list.add(2);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+
+        HashMap<String, Integer> map = new HashMap<String, Integer>();
+        map.put("Red", 1);
+        map.put("Green", 2);
+        map.put("Black", 3);
+        map.put("White", 4);
+
+        putIfAbsent(map,"Yellow",6);
+
+        System.out.println(list);
+        for (String i : map.keySet()) {
+            System.out.println(i);
+        }
+        for (Integer i : map.values()) {
+            System.out.println(i);
+
+        }
+    }
+
+
+    public static void putIfAbsent(HashMap<String, Integer> map,String key, int value){
+        map.putIfAbsent(key,value);
     }
 }
